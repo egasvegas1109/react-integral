947fb3c809e751a27150ec3befca53343372134c
EgasVegas <egor1109_45@mail.ru>
2022-09-21 17:29:28 +0300

    сохранение состояний в переменные

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-function App() {
-  return (
-    <div>
-      <header>
-        <h3>Расчёт интеграла</h3>
-        <p>Лабораторная №1</p>
-        <hr></hr>
-      </header>
+import React from "react";
 
-      <body>
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { upperInt: null, lowerInt: null, stepInt: null };
+    this.upper = this.upper.bind(this);
+    this.lower = this.lower.bind(this);
+    this.step = this.step.bind(this);
+  }
+
+  upper(e) {
+    this.setState({ upperInt: e.target.value }, function () {
+      /* Получаем измененное значение state */
+      console.log("Верхний предел " + this.state.upperInt);
+    });
+  }
+
+  lower(e) {
+    this.setState({ lowerInt: e.target.value }, function () {
+      /* Получаем измененное значение state */
+      console.log("Нижний предел " + this.state.lowerInt);
+    });
+  }
+
+  step(e) {
+    this.setState({ stepInt: e.target.value }, function () {
+      /* Получаем измененное значение state */
+      console.log("Число разбиений " + this.state.stepInt);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <header>
+          <h3>Расчёт интеграла</h3>
+          <p>Лабораторная №1</p>
+          <hr></hr>
+        </header>
+
         <div className="inputs">
           <div>
             <p>Верхний предел</p>
-            <input placeholder="Введите значение"></input>
+            <input placeholder="Введите значение" onChange={this.upper}></input>
           </div>
 
           <div>
             <p>Нижний предел</p>
-            <input placeholder="Введите значение"></input>
+            <input placeholder="Введите значение" onChange={this.lower}></input>
           </div>
 
           <div>
             <p>Число разбиений</p>
-            <input placeholder="Введите значение"></input>
+            <input placeholder="Введите значение" onChange={this.step}></input>
           </div>
         </div>
         <hr></hr>
@@ -33,10 +63,7 @@ function App() {
             <p>Интеграл =</p>
           </div>
         </div>
-
-      </body>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
-export default App;
